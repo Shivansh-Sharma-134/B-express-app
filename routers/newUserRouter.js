28b1268c75f5aaeaa9c46bf1b0dd96f88972ca34
shivansh sharma <105637680+Shivansh-Sharma-134@users.noreply.github.com>
2025-05-03 18:54:45 +0530

    added validation to form

--- routers/newUserRouter.js
+++ routers/newUserRouter.js
@@ -2,11 +2,29 @@ const {Router} = require("express");
 const users = require("../data/db");
 const newUserRouter = Router();
 
+const { body, validationResult } = require("express-validator");
+
+const validateUser = [
+  body("firstNameInput").trim().isAlpha().withMessage('must only contain letters').isLength({min: 1,max: 10}).withMessage('must be within 1 to 10 characters'),
+  body("lastNameInput").trim().isAlpha().withMessage('must only contain letters').isLength({min: 1,max: 10}).withMessage('must be within 1 to 10 characters')
+]
+
 newUserRouter.get("/",(req,res)=>{
+    
     res.render("form")
 })
  
-newUserRouter.post("/",(req,res)=>{
+newUserRouter.post("/",validateUser,(req,res)=>{
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(400).render("form", {
+          title: "Create user",
+          errors: errors.array(),
+          old: req.body 
+        });
+      }
+
     users.push({
         firstName: req.body.firstNameInput,
         lastName: req.body.lastNameInput,
